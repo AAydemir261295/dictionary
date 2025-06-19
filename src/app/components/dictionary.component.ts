3dde61581b1738859237abd1f73582637600bd04
Ayberk Aydemir <onlybriefcase@gmail.com>
2025-06-19 11:19:27 +0300

    edit dictionary component

--- src/app/components/dictionary.component.ts
+++ src/app/components/dictionary.component.ts
@@ -1,9 +1,10 @@
 import { Component } from "@angular/core";
 import { DictionaryServiceProvider } from "./service/dictionary.provider";
+import { LangInput } from "./langInput/langInput.component";
 
 @Component({
     selector: 'dictionary',
-    imports: [],
+    imports: [LangInput],
     providers: [DictionaryServiceProvider],
     templateUrl: './dictionary.template.html',
     styleUrl: './dictionary.style.scss'
