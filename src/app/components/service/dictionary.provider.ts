2199ff747b7feea2e791b05bc46b23ec332f9a06
Ayberk Aydemir <onlybriefcase@gmail.com>
2025-06-19 11:09:46 +0300

    edit dictionary provider

--- src/app/components/service/dictionary.provider.ts
+++ src/app/components/service/dictionary.provider.ts
@@ -1,11 +1,12 @@
 import { HttpClient } from "@angular/common/http";
 import { DictionaryService } from "./dictionary.service";
 
-const ServiceFactory = (http: HttpClient) => new DictionaryService(http);
+const apiKey = "dict.1.1.20250619T033726Z.6840cca2c782c33b.93de26b47845a21b1a52064c909fef1f22bc97ab";
+const ServiceFactory = (http: HttpClient) => new DictionaryService(http, apiKey);
 
 
 export const DictionaryServiceProvider = {
     provide: DictionaryService,
     useFactory: ServiceFactory,
-    deps: [HttpClient]
+    deps: [HttpClient, apiKey]
 }
