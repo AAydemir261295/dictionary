9f9aa0a4628860f2d9caa1e8c7a48021350a0be8
Ayberk Aydemir <onlybriefcase@gmail.com>
2025-06-19 16:00:27 +0300

    edit dictionary component

--- src/app/components/dictionary.component.ts
+++ src/app/components/dictionary.component.ts
@@ -1,15 +1,49 @@
-import { Component } from "@angular/core";
+import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
 import { DictionaryServiceProvider } from "./service/dictionary.provider";
 import { LangInput } from "./langInput/langInput.component";
+import { FormControl, FormGroup, ReactiveFormsModule } from "@angular/forms";
+import { Language } from "./service/models/Language";
+import { DictionaryService } from "./service/dictionary.service";
 
 @Component({
     selector: 'dictionary',
-    imports: [LangInput],
+    imports: [ReactiveFormsModule, LangInput],
     providers: [DictionaryServiceProvider],
     templateUrl: './dictionary.template.html',
-    styleUrl: './dictionary.style.scss'
+    styleUrl: './dictionary.style.scss',
+    standalone: true,
 })
-export class DictionaryComponent {
+export class DictionaryComponent implements OnInit {
     title = 'dictionary';
+
+    constructor(private service: DictionaryService) {
+        this.items = this.service.getLanguages();
+    }
+
+    items: Language[] = []
+
+
+    form: FormGroup = new FormGroup({
+        text: new FormControl(""),
+        langInput: new FormControl(""),
+        langOutput: new FormControl("")
+    })
+
+    formAction() {
+    }
+
+    timeOut: any = null;
+    disableFocus(btn: HTMLButtonElement) {
+        if (this.timeOut == null) {
+            this.timeOut = setTimeout(() => {
+                btn.blur();
+                this.timeOut = null;
+            }, 300);
+        }
+    }
+
+    ngOnInit(): void {
+    }
+
 
 }
