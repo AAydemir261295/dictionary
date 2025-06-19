ae2b0e64a0bb8987c0546a17d2bb2fb7266770a2
Ayberk Aydemir <onlybriefcase@gmail.com>
2025-06-19 08:58:26 +0300

    add import

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DictionaryComponent } from './components/dictionary.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DictionaryComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'dictionary';
+
+  
 }
