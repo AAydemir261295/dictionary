20796757770f2c862ec2664539e38aa90edc4458
Ayberk Aydemir <onlybriefcase@gmail.com>
2025-06-20 12:48:12 +0300

    edit input animations logic

--- src/app/components/langInput/langInput.component.ts
+++ src/app/components/langInput/langInput.component.ts
@@ -1,8 +1,9 @@
-import { Component, Input } from "@angular/core";
+import { Component, ElementRef, HostListener, Inject, input, Input, ViewChild } from "@angular/core";
 import { ControlValueAccessor, FormControl, NG_VALUE_ACCESSOR, ReactiveFormsModule } from "@angular/forms";
 import { Subscription } from "rxjs";
 import { Language } from "../service/models/Language";
 import { DictionaryService } from "../service/dictionary.service";
+import { DOCUMENT } from "@angular/common";
 
 
 
@@ -14,7 +15,7 @@ import { DictionaryService } from "../service/dictionary.service";
             provide: NG_VALUE_ACCESSOR,
             multi: true,
             useExisting: LangInput
-        }
+        },
     ],
     templateUrl: './langInput.template.html',
     styleUrl: './langInput.style.scss'
@@ -29,22 +30,26 @@ export class LangInput implements ControlValueAccessor {
             if (val == "") {
                 this.resetItems();
             } else {
+                this.showDdown = true;
                 this.items = this.items.filter((v) => v.fullName.match(new RegExp(val)));
             }
         });
     }
 
+    showDdown: boolean = false;
     items: Language[] = [];
-
     inputControl: FormControl = new FormControl();
     sub: Subscription;
 
 
-    expand() { }
+    show() {
+        this.showDdown = true;
+    }
 
     select(value: Language) {
         this.writeValue(value.fullName);
         this.resetItems();
+        this.showDdown = false;
     }
 
     resetItems() {
@@ -65,5 +70,13 @@ export class LangInput implements ControlValueAccessor {
         this.onChange = fn;
     }
 
-    registerOnTouched(fn: any): void { }
+    registerOnTouched(fn: any): void {
+    }
+
+
+    @HostListener("focusout", ['$event.target'])
+    focusOut(e: any) {
+        this.showDdown = false;
+    }
+
 }
