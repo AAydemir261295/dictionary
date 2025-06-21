9884f5e487fff40784b92ec6dd887d468f77c950
Ayberk Aydemir <onlybriefcase@gmail.com>
2025-06-21 14:38:20 +0300

    add translateResult component

---
+++ src/app/components/translateResult/translateResult.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'translate-result',
+    imports: [],
+    providers: [],
+    templateUrl: './translateResult.template.html',
+    styleUrl: './translateResult.style.scss',
+    standalone: true,
+})
+export class TranslateResultComponent {
+    constructor() { }
+
+}
