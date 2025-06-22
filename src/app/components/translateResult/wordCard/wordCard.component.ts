082ae0100a3925a990914d869da1ba9b18fbd2ae
Ayberk Aydemir <onlybriefcase@gmail.com>
2025-06-22 17:46:51 +0300

    add WordCard component

---
+++ src/app/components/translateResult/wordCard/wordCard.component.ts
@@ -0,0 +1,33 @@
+import { Component, Input } from "@angular/core";
+import { TranslateRow } from "../../../services/models/Result";
+
+@Component({
+    selector: 'word-card',
+    imports: [],
+    providers: [],
+    templateUrl: './wordCard.template.html',
+    styleUrl: './wordCard.style.scss',
+    standalone: true,
+})
+export class WordCardComponent {
+
+    wordTypes: { [key: string]: string } = {
+        "noun": "существительное",
+        "verb": "прилагательное",
+        "adjective": "глагол",
+    }
+
+    @Input()
+    data: TranslateRow | undefined;
+
+    // @Input()
+    // isSynonym: boolean | undefined;
+
+
+
+
+
+
+
+
+}
